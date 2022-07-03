dafb4094c765a7a0faf5ff6202cc685a416adb62
ncrispino <ncrispino@wustl.edu>
2022-07-03 14:22:21 -0500

    map works

--- dash_model.py
+++ dash_model.py
@@ -1,8 +1,10 @@
 # Note: using Github Copilot for the first time in a file.
 # Also using https://towardsdatascience.com/build-a-machine-learning-simulation-tool-with-dash-b3f6fd512ad6 heavily as a guide.
 # Thanks to the author Pierre-Louis Bescond. Check out the article for more information.
+# Using https://towardsdatascience.com/3-easy-ways-to-make-your-dash-application-look-better-3e4cfefaf772 for styling advice.
 
 from distutils.log import debug
+from re import M
 from matplotlib.pyplot import figure
 import pandas as pd
 import numpy as np
@@ -12,6 +14,8 @@ from dash import dcc
 from dash import html
 import dash_daq as daq
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
 import plotly.figure_factory as ff
 import plotly.express as px
 
@@ -23,7 +27,8 @@ import joblib
 model = joblib.load('booster_model.joblib')
 
 # Create dash app
-app = dash.Dash()
+app = dash.Dash(external_stylesheets=[dbc.themes.LUX])
+load_figure_template('LUX')
 
 # Page structure will be:
     # Map of US by county shaded based on output from fitted model
@@ -35,62 +40,88 @@ app = dash.Dash()
     # Slider to update student body size
 
 # apply basic HTML layout
-app.layout = html.Div(style={'textAlign': 'center', 'width': '100%', 'font-family': 'Verdana'}, 
+app.layout = html.Div(style={'textAlign': 'center', 'width': '90%', 'margin-left': '50px', 
+'margin-right': '50px', 'margin-top': '25px', 'margin-bottom': '25px'},
 children=[
-    html.H1('Booster Mandate Predictor'),
-    # show map from callback
-    dcc.Graph(id='map'),
+    dbc.Row(
+        dbc.Col(
+            html.H1('Booster Mandate Predictor'),
+        )
+    ),
+    dbc.Row(
+        dbc.Col(
+            # show map from callback
+            dcc.Graph(id='map'),
+            # style = {'margin-left': '25px', 'margin-right': '25px'}
+        )
+    ),
     # make note that says that counties that are not shaded did not have enough data to run the model
-    html.P('Counties that are not shaded did not have enough data to run the model'),  
-    html.H3('Type of School'),
-    dcc.RadioItems(
-        id='type',
-        options=[
-            {'label': 'Public', 'value': 'Public'},
-            {'label': 'Private', 'value': 'Private'},
-        ],
-        value='Public',
-        labelStyle={'display': 'inline-block'},
-    ),            
-    html.H3('Update Ranking'),
-    dcc.Slider(
-        id='ranking',
-        min=0,
-        max=400,
-        step=1,
-        value=100,
-        marks={
-            0: '0',
-            20: '20',
-            100: '100',
-            200: '200',
-            298: '298',
-            400: '400',
-        },
+    dbc.Row(
+        [dbc.Col('Type of School'),
+        dbc.Col('Ranking'),
+        dbc.Col('Announce Date'),
+        dbc.Col('Student Body Size')],
+        style={'text-decoration': 'underline'}
     ),
-    html.H3('Update Announce Date'),
-    dcc.Slider(
-        id='announce_date',
-        min=0,
-        max=200,
-        step=1,
-        value=20,
-        marks={i: str(i) for i in range(0, 201, 10)}
+    dbc.Row(
+        [dbc.Col(
+            [dcc.RadioItems(
+                id='type',
+                options=[
+                    {'label': 'Public', 'value': 'Public'},
+                    {'label': 'Private', 'value': 'Private'},
+                ],
+                value='Public',
+                labelStyle={'display': 'inline-block'},
+                inputStyle={'margin-left': '10px', 'margin-right': '10px'},
+                )]
+        ),
+        dbc.Col(                      
+            [dcc.Slider(
+                id='ranking',
+                min=0,
+                max=4,
+                step=1,
+                value=3,
+                marks={
+                    0: '0-19',
+                    1: '20-99',
+                    2: '100-99',
+                    3: '200-98',
+                    4: '299+'                    
+                }
+            )]
+        ),
+        dbc.Col(            
+            [dcc.Slider(
+                id='announce_date',
+                min=0,
+                max=200,
+                step=1,
+                value=20,
+                marks={i: str(i) for i in range(0, 201, 50)}
+            )]
+        ),
+        dbc.Col(            
+            [dcc.Slider(
+                id='student_body_size',
+                min=0,
+                max=70000,
+                step=100,
+                value=10000,
+                marks={i: str(i//1000) + 'k' for i in range(0, 70001, 10000)}
+            )]
+        )]
     ),
-    html.H3('Update Student Body Size'),
-    dcc.Slider(
-        id='student_body_size',
-        min=0,
-        max=70000,
-        step=100,
-        value=10000,
-        marks={i: str(i) for i in range(0, 70001, 10000)}
+    dbc.Row(
+        html.P('Counties that are not shaded did not have enough data to run the model', style={'font-size': '12px', 'font-style': 'italic'}),
+        style={'margin-top': '10px'},
     ),
-    html.H3('Distribution of Values'),
-    dcc.Graph(id='distribution-of-values'),
+    # html.H3('Distribution of Values'),
     # show the number of boosters from callback
     html.H3('Number of Boosters'),
-    html.H4(id='number-of-boosters'),      
+    html.H4(id='number-of-boosters'),    
+    dcc.Graph(id='distribution-of-values'),  
 ])
 
 # Create callback for histogram using all the values provided and the update_prediction method below
@@ -143,7 +174,7 @@ def update_prediction(type, ranking, announce_date, student_body_size):
         opacity=0.5,
         labels={'STCOUNTYFP': 'County'}
     )
-    map_fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    map_fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0}, legend={'orientation': 'h', 'y': 1, 'x': 0.5, 'xanchor': 'center', 'yanchor': 'top'})
     return college_data_clean['booster'].sum(), bar_fig, map_fig
 
 if __name__ == '__main__':
